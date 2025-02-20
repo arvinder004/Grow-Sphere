5eb22642e8395b0f3c2f0b2a77f52d00965c4446
Burhanuddin saifee <burhansaifee2003@gmail.com>
2025-02-20 20:16:13 +0530

    commit

---
+++ React/Project/3-myntra-react-clone/src/components/Bag.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header"
+import Footer from "./Footer"
+const Bag = () => {
+
+    return <>
+        <Header></Header>
+    <main>
+      <div class="bag-page">
+        <div class="bag-items-container">
+        </div>
+        <div class="bag-summary">
+        </div>
+
+      </div>
+    </main>
+    <Footer></Footer>
+     </>
+}
+export default Bag
