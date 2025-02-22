185814149bca7c33f3402cd1f5105bb5990e1c25
Burhanuddin saifee <burhansaifee2003@gmail.com>
2025-02-22 20:32:37 +0530

    commit

---
+++ digital-agency-main/src/components/Hero/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import yellowCar from "../../assets/website/team.png";
+
+const Hero = () => {
+  return (
+    <div className="dark:bg-gray-950 dark:text-white duration-300 ">
+      <div className="container min-h-[620px] flex mt-10 sm:mt-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 place-items-center">
+          
+          <div data-aos="zoom-in" className="order-1 sm:order-2 relative">
+            <img
+              src={yellowCar}
+              alt=""
+              className="w-full sm:max-w-[280px] md:max-w-[430px]"
+            />
+          </div>
+
+          
+          <div className="space-y-5 order-2 sm:order-1 xl:pr-40 ">
+            <h1
+              data-aos="fade-up"
+              className="text-4xl sm:text-5xl font-semibold"
+              style={{ lineHeight: 1.2 }}
+            >
+              Building Brands with{" "}
+              <span className="text-primary">GROW SPHERE</span>
+            </h1>
+            <p data-aos="fade-up" data-aos-delay="300">
+              Your partner in navigating the ever-evolving landscape of digital
+              marketing. From conceptualization to execution, we craft tailored
+              solutions that drive results and elevate your brand to new
+              heights.
+            </p>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
